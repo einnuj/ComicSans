8eb21d153b64552c9f46d818a89bd3623d83af50
einnuJ <junnie.sim@stonybrook.edu>
2016-04-07 12:52:57 -0400

    ComicMetadata is about half complete.

--- src/main/java/model/metadata/ComicMetadata.java
+++ src/main/java/model/metadata/ComicMetadata.java
@@ -1,8 +1,22 @@
 package model.metadata;
 
+import model.metadata.fields.Comment;
+import model.metadata.fields.Favorite;
+import model.metadata.fields.Like;
+
+import java.util.List;
+
 /**
  * A class that represents the Metadata of a Comic.
  * Created by einnuj on 4/7/2016.
  */
 public class ComicMetadata {
+    private List<Like> likeList;
+    private List<Favorite> favoriteList;
+    private List<Comment> commentList;
+    private GenreEnum genre;
+    private String description;
+
+    // This should be a User
+    private String author;
 }
--- src/main/java/model/metadata/fields/Bookmark.java
+++ src/main/java/model/metadata/fields/Bookmark.java
@@ -7,7 +7,7 @@ import com.google.appengine.repackaged.com.google.api.client.util.DateTime;
  * Created by einnuj on 4/7/2016.
  */
 public class Bookmark extends AbstractField {
-    
+
     // Like Favorite and such, there are no fields/methods necessary.
 
     public Bookmark(String userOrigin, String comicTarget, DateTime dateTime) {

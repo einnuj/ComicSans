d6079e728b25338012c05968920d0dbd2d8b5d11
einnuJ <junnie.sim@stonybrook.edu>
2016-04-07 22:03:10 -0400

    ComicMetaData completely fleshed out. Deciding whether or not to include stat-generating stats as methods (avgRating, totalLikes, etc).

--- src/main/java/model/metadata/ComicMetadata.java
+++ src/main/java/model/metadata/ComicMetadata.java
@@ -16,7 +16,7 @@ public class ComicMetadata {
     private List<Like> likeList;
     private List<Favorite> favoriteList;
     private List<Comment> commentList;
-    private Rating rating;
+    private List<Rating> ratingList;
     private GenreEnum genre;
     private String description;
 
@@ -27,6 +27,7 @@ public class ComicMetadata {
         likeList = new ArrayList<Like>();
         favoriteList = new ArrayList<Favorite>();
         commentList = new ArrayList<Comment>();
+        ratingList = new ArrayList<Rating>();
         genre = GenreEnum.UNLISTED;
         description = "";
         this.author = author;
@@ -45,6 +46,8 @@ public class ComicMetadata {
     public List<Comment> getCommentList() {
         return commentList;
     }
+
+    public List<Rating> getRatingList() { return ratingList; }
 
     public GenreEnum getGenre() {
         return genre;

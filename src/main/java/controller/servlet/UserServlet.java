2c963592e698ba9a36fd043cf9d6a6e12acbe4bf
einnuj <junnie.sim@stonybrook.edu>
2016-04-12 19:45:28 -0400

    Sets an attribute "user" in the request so that it's accessible to the page that requested it. Web.xml configured so that UserServlet is invoked only in /profile.jsp
    Signed-off-by: einnuj <junnie.sim@stonybrook.edu>

--- src/main/java/controller/servlet/UserServlet.java
+++ src/main/java/controller/servlet/UserServlet.java
@@ -18,6 +18,7 @@ public class UserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         User newUser = new MockUserController().genMockUser("Junnie");
+        req.setAttribute("user", newUser);
     }
 
     @Override

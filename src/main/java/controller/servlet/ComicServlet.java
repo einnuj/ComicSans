e37d3a3c0ba216bb1dcc0d66cad2bc221fe49e68
einnuj <junnie.sim@stonybrook.edu>
2016-04-12 20:20:27 -0400

    Created a basic HttpServlet, ComicServlet.
    Signed-off-by: einnuj <junnie.sim@stonybrook.edu>

---
+++ src/main/java/controller/servlet/ComicServlet.java
@@ -0,0 +1,24 @@
+package controller.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * A Servlet that will respond with a WebComic class.
+ * Created by einnuj on 4/12/2016.
+ */
+public class ComicServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}

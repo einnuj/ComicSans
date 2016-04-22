73f211c473f0edfca72fb80655ab6a0fa20b18c1
einnuJ <junnie.sim@stonybrook.edu>
2016-04-21 22:37:30 -0400

    Shifted Mock Comic functions to MockComicServlet; cleared ComicServlet to prepare for generating real WebComics.

--- src/main/java/controller/servlet/ComicServlet.java
+++ src/main/java/controller/servlet/ComicServlet.java
@@ -1,10 +1,5 @@
 package controller.servlet;
 
-import controller.mock.MockComicController;
-import model.comics.ComicPage;
-import model.comics.WebComic;
-import utilities.JsonHelper;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -20,29 +15,11 @@ public class ComicServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        int page = Integer.parseInt(req.getParameter("page"));
-        int chapter = Integer.parseInt(req.getParameter("chapter"));
-        String genre = req.getParameter("genre");
-        String imgFile = req.getParameter("imgFile");
-        String title = req.getParameter("title");
-
-
-        System.out.println(title);
-        System.out.println(genre);
-        System.out.println(chapter);
-        System.out.println(page);
-
-        ComicPage ourPage = new ComicPage(title, imgFile, page, chapter, genre);
-
 
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        WebComic webComic = new MockComicController().genWebComic("Comic " +
-                "Sam", "Junnie");
-        String comicInJson = JsonHelper.objectToJson(webComic);
-
-        resp.getWriter().write(comicInJson);
+        
     }
 }

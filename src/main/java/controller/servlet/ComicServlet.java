d57b7a5b48da6b89aecf7350b36e11c4a5cf7d9c
einnuJ <junnie.sim@stonybrook.edu>
2016-04-21 22:55:39 -0400

    Moved queryForUser to a data/UserAccess class.

--- src/main/java/controller/servlet/ComicServlet.java
+++ src/main/java/controller/servlet/ComicServlet.java
@@ -20,6 +20,6 @@ public class ComicServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
+
     }
 }

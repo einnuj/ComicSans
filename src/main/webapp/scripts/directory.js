e1b8a76207f1d45639c8ae4a72f71a17fe247c16
einnuj <junnie.sim@stonybrook.edu>
2016-05-05 17:07:27 -0400

    Skeleton of filterComics() function. Attempting to get text of dropdown-menu, thereby eliminating the need to pass the text (DRY).

--- src/main/webapp/scripts/directory.js
+++ src/main/webapp/scripts/directory.js
@@ -1,7 +1,35 @@
 /**
+ * directory JS scripts for sorting/filtering comics
  * Created by Junnie on 5/5/2016.
  */
 
 function sortComics(filter) {
     
+}
+
+function filterComics() {
+    var filter = $('.dropdown-menu :selected').text();
+
+    switch(filter) {
+        case 'Action':
+            break;
+        case 'Comedy':
+            break;
+        case 'Drama':
+            break;
+        case 'Fantasy':
+            break;
+        case 'Gaming':
+            break;
+        case 'Horror':
+            break;
+        case 'Romance':
+            break;
+        case 'Sports':
+            break;
+        case 'Thriller':
+            break;
+        default:
+            break;
+    }
 }

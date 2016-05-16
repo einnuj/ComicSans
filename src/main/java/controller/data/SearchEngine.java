82bf2a21d93c7b5e766cc6f2691eb13015262f4e
Junnie Sim <junniesim91@gmail.com>
2016-05-16 11:28:19 -0400

    Beginning searchComics; edited search to include a String search query

--- src/main/java/controller/data/SearchEngine.java
+++ src/main/java/controller/data/SearchEngine.java
@@ -3,12 +3,11 @@ package controller.data;
 import model.comics.WebComic;
 import model.users.User;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class SearchEngine {
 
-    public static List<Map> search(List<WebComic> comicsList, List<User> userList) {
+    public static List<Map> search(List<WebComic> comicsList, List<User> userList, String query) {
         // Returns a Map<id, binToDecNumber> where the binToDecNumber is a decimal number, but when converted to binary, represents which of the following had search hits:
         // Comic Name, Chapter Names, Page Names, Author Name, Bio, Comments
 
@@ -24,5 +23,13 @@ public class SearchEngine {
          */
 
         return null;
+    }
+
+    private static Map<Long, Integer> searchComics(List<WebComic> comicsList, String query) {
+        Map<Long, Integer> hits = new HashMap<Long, Integer>();
+
+        
+
+        return hits;
     }
 }

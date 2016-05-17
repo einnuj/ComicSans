6b9ce409f5307b62ff61abf6acb2c8543d29063c
Junnie Sim <junniesim91@gmail.com>
2016-05-17 19:22:11 -0400

    Hit a rut; should I force the page to accept jstl through clever Servlet powers, or just do multiple ajax calls to get the comics themselves? Either way, use of AllComics is expected.

--- src/main/webapp/scripts/search.js
+++ src/main/webapp/scripts/search.js
@@ -4,6 +4,12 @@ function searchFunc() {
         type: "POST",
         data: {"search" : $("#search-text").val(), "jsp" : true},
         success: function(response) {
+            var comicString = "comics";
+            var commentString = "comments";
+            var userString = "users";
+            addToHTML(response[0], comicString);
+            addToHTML(response[1], commentString);
+            addToHTML(response[2], userString);
             var comicResults = response[0];
             var commentResults = response[1];
             var userResults = response[2];
@@ -15,6 +21,33 @@ function searchFunc() {
     });
     
     console.log("SUCCESS");
+}
+
+function addToHTML(resultMap, typeString) {
+    switch (typeString) {
+        case "comics":
+            var comicsMap = getComics(resultMap);
+            populateComicsDiv(resultMap);
+            break;
+        case "comments":
+            break;
+        case "users":
+            break;
+        default:
+            console.log("ERROR");
+    }
+}
+
+function getComics(resultMap) {
+    $.ajax({
+        url: ""
+    })
+}
+
+function populateComicsDiv(finalComicsMap) {
+    var comicsDiv = $("#search-comics > ul");
+
+
 }
 
 function check_session() {

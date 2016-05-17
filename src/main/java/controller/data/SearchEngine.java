195ce67a30ccbe3d5aede67ba976d897a90d6ad6
Junnie Sim <junniesim91@gmail.com>
2016-05-16 21:35:58 -0400

    Completed collapseChapterAndPageNames and searchComic now returns the relevant binary number as a char[].

--- src/main/java/controller/data/SearchEngine.java
+++ src/main/java/controller/data/SearchEngine.java
@@ -3,6 +3,7 @@ package controller.data;
 import model.comics.ComicChapter;
 import model.comics.ComicPage;
 import model.comics.WebComic;
+import model.metadata.ComicMetadata;
 import model.users.User;
 import org.ahocorasick.trie.Emit;
 import org.ahocorasick.trie.Trie;
@@ -41,21 +42,47 @@ public class SearchEngine {
                 .caseInsensitive()
                 .addKeyword("casing")
                 .build();
-        Collection<Emit> emits = trie.parseText("CaSiNg");
+        Emit emits = trie.firstMatch("not here");
 
         return hits;
     }
 
-    private static void searchComic(WebComic comic, Trie trie) {
-        byte[] results = new byte[6];
+    private static char[] searchComic(WebComic comic, Trie trie) {
+        char[] results = new char[]{'0', '0', '0', '0', '0'};
 
         List<StringBuilder> collapsedNames = collapseChapterAndPageNames(comic);
+        ComicMetadata metadata = comic.getMetadata();
 
         Emit searchComicName = trie.firstMatch(comic.getName());
         Emit searchChapterNames = trie.firstMatch(collapsedNames.get(0));
         Emit searchPageNames = trie.firstMatch(collapsedNames.get(1));
+        Emit searchAuthorName = trie.firstMatch(metadata.getAuthor());
+        Emit searchBio = trie.firstMatch(metadata.getBio());
+
+        if (searchComicName != null) {
+            results[0] = '1';
+        }
+        if (searchChapterNames != null) {
+            results[1] = '1';
+        }
+        if (searchPageNames != null) {
+            results[2] = '1';
+        }
+        if (searchAuthorName != null) {
+            results[3] = '1';
+        }
+        if (searchBio != null) {
+            results[4] = '1';
+        }
+
+        return results;
     }
 
+    /**
+     * Takes a WebComic and condenses the name of the Comic, the Comics' Chapters, and the Chapters' Pages, into one.
+     * @param comic - the target WebComic
+     * @return a List of StringBuilder: [0] is the collapsed Chapter names, and [1] is the collapsed Page names.
+     */
     private static List<StringBuilder> collapseChapterAndPageNames(WebComic comic) {
         ArrayList<StringBuilder> results = new ArrayList<StringBuilder>();
         StringBuilder collapsedChapterNames = new StringBuilder();

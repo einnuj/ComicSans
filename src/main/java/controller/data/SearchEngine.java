3956707dcb6995698fed158f37e5d0fabab55ade
Junnie Sim <junniesim91@gmail.com>
2016-05-16 20:16:20 -0400

    Starting working on searchComics(); looks like the name portions of Comics is done parsing.

--- src/main/java/controller/data/SearchEngine.java
+++ src/main/java/controller/data/SearchEngine.java
@@ -1,5 +1,7 @@
 package controller.data;
 
+import model.comics.ComicChapter;
+import model.comics.ComicPage;
 import model.comics.WebComic;
 import model.users.User;
 import org.ahocorasick.trie.Emit;
@@ -29,7 +31,9 @@ public class SearchEngine {
         Map<Long, Integer> hits = new HashMap<Long, Integer>();
 
         for (WebComic comic : comicsList) {
-            
+            Trie trie = Trie.builder().removeOverlaps().caseInsensitive().addKeyword(query).build();
+
+            searchComic(comic, trie);
         }
 
         // The Collection<Emit> that comes as the result of the Trie parse contains the keyword, and the start/end index of where it was found.
@@ -40,5 +44,33 @@ public class SearchEngine {
         Collection<Emit> emits = trie.parseText("CaSiNg");
 
         return hits;
+    }
+
+    private static void searchComic(WebComic comic, Trie trie) {
+        byte[] results = new byte[6];
+
+        List<StringBuilder> collapsedNames = collapseChapterAndPageNames(comic);
+
+        Emit searchComicName = trie.firstMatch(comic.getName());
+        Emit searchChapterNames = trie.firstMatch(collapsedNames.get(0));
+        Emit searchPageNames = trie.firstMatch(collapsedNames.get(1));
+    }
+
+    private static List<StringBuilder> collapseChapterAndPageNames(WebComic comic) {
+        ArrayList<StringBuilder> results = new ArrayList<StringBuilder>();
+        StringBuilder collapsedChapterNames = new StringBuilder();
+        StringBuilder collapsedPageNames = new StringBuilder();
+
+        for (ComicChapter chapter : comic.getChildMediaList()) {
+            collapsedChapterNames.append(chapter.getName());
+            for (ComicPage page : chapter.getChildMediaList()) {
+                collapsedPageNames.append(page.getName());
+            }
+        }
+
+        results.add(collapsedChapterNames);
+        results.add(collapsedPageNames);
+
+        return results;
     }
 }

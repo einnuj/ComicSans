d969292fc5525ab1910fe73bc5a4e629aad0fc7c
einnuJ <junnie.sim@stonybrook.edu>
2016-05-18 05:12:44 -0400

    Accidentally bracketed out the new function. Corrected

--- src/main/java/controller/servlet/Upload.java
+++ src/main/java/controller/servlet/Upload.java
@@ -31,161 +31,158 @@ import java.util.Map;
 /**
  * Created by cherrinkim on 5/15/16.
  */
-public class Upload extends HttpServlet{
+public class Upload extends HttpServlet {
 
-     private BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+    private BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
 
-        @Override
-        public void doPost(HttpServletRequest req, HttpServletResponse resp)
-                throws ServletException, IOException {
-            String action = req.getParameter("action");
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String action = req.getParameter("action");
 
-            switch(action) {
-                case "CREATE COMIC":
+        switch (action) {
+            case "CREATE COMIC":
 
-                    com.google.appengine.api.users.User googleUser = UserServiceFactory.getUserService().getCurrentUser();
+                com.google.appengine.api.users.User googleUser = UserServiceFactory.getUserService().getCurrentUser();
 
-                    if (googleUser != null) {
-                        try {
-                            User author = UserAccess.queryForUser(googleUser.getUserId());
-                            String name = req.getParameter("title");
-                            String summary = req.getParameter("summary");
-                            String genre = req.getParameter("genre");
-
-                            Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(req);
-
-                            WebComic newComic;
-
-                            if (author == null) {
-                                throw new UserNotFoundException(googleUser.getUserId());
-                            }
-                            if (name == null) {
-                                throw new ParameterNotFoundException("name");
-                            }
-                            if (summary == null) {
-                                throw new ParameterNotFoundException("summary");
-                            }
-                            if (genre == null) {
-                                throw new ParameterNotFoundException("genre");
-                            }
-                            else {
-                                newComic = new WebComic(name, author.getGoogleId(), genre);
-
-                            }
-
-                            //setting parameters
-                            String cover = blobs.get("file").get(0).getKeyString();
-                            newComic.getMetadata().setCoverImage(cover);
-                            newComic.getMetadata().setBio(summary);
-
-                            ComicChapter newChapter = new ComicChapter("Comic");
-                            newComic.addToChildMediaList(newChapter);
-
-                            ObjectifyHelper.save(newComic);         // Gets us the Long id
-                            newComic.reload();
-
-                            // Update User!
-                            author.getMetadata().addToComicsCreated(newComic);
-
-                            ObjectifyHelper.save(newComic);
-                            ObjectifyHelper.save(author);
-
-                            newChapter.reload();
-                            newComic.reload();
-
-                            resp.getWriter().write(newComic.getId().toString());
-                            break;
-
-                        }
-                        catch (NonUniqueGoogleIdException | UserNotFoundException ex) {
-                            resp.getWriter().write("{\"error\":" + ex.getMessage() + "}");
-
-                            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-                        }
-                        catch (ParameterNotFoundException ex) {
-                            resp.getWriter().write("{\"error\":" + ex.getMessage() + "}");
-
-                            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
-                        }
-                    }
-                    else {
-                        resp.getWriter().write("{\"error\":\"No User Logged In\"}");
-
-                        resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-                    }
-                    break;
-
-                case "ADD ISSUE":
+                if (googleUser != null) {
                     try {
+                        User author = UserAccess.queryForUser(googleUser.getUserId());
+                        String name = req.getParameter("title");
+                        String summary = req.getParameter("summary");
+                        String genre = req.getParameter("genre");
+
                         Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(req);
 
-                        String title = req.getParameter("issueTitle");
-                        String comicId = req.getParameter("comicId");
+                        WebComic newComic;
 
-                        if (title == null) {
-                            throw new ParameterNotFoundException("issueTitle");
+                        if (author == null) {
+                            throw new UserNotFoundException(googleUser.getUserId());
                         }
-                        if (comicId == null) {
-                            throw new ParameterNotFoundException("comicId");
+                        if (name == null) {
+                            throw new ParameterNotFoundException("name");
                         }
-
-                        Long id = Long.valueOf(comicId);
-                        WebComic comic = ComicAccess.queryForComic(id);
-                        ComicChapter myChapter = comic.getChildMediaList().get(0);
-
-                        if (!isUniqueName(myChapter.getChildMediaList(), title)) {
-                            throw new RepeatTitleException(title);
+                        if (summary == null) {
+                            throw new ParameterNotFoundException("summary");
                         }
+                        if (genre == null) {
+                            throw new ParameterNotFoundException("genre");
+                        } else {
+                            newComic = new WebComic(name, author.getGoogleId(), genre);
 
-                        for (String n : blobs.keySet()) {
-                            String file = blobs.get(n).get(0).getKeyString();
-                            ComicPage newPage = new ComicPage(title, file);
-                            myChapter.addToChildMediaList(newPage);
                         }
 
-                        myChapter.reload();
-                        ObjectifyHelper.save(comic);
+                        //setting parameters
+                        String cover = blobs.get("file").get(0).getKeyString();
+                        newComic.getMetadata().setCoverImage(cover);
+                        newComic.getMetadata().setBio(summary);
 
-                        comic.reload();
-                        comic.getMetadata().reload();
-                        resp.getWriter().write(comic.getId().toString());
+                        ComicChapter newChapter = new ComicChapter("Comic");
+                        newComic.addToChildMediaList(newChapter);
 
+                        ObjectifyHelper.save(newComic);         // Gets us the Long id
+                        newComic.reload();
+
+                        // Update User!
+                        author.getMetadata().addToComicsCreated(newComic);
+
+                        ObjectifyHelper.save(newComic);
+                        ObjectifyHelper.save(author);
+
+                        newChapter.reload();
+                        newComic.reload();
+
+                        resp.getWriter().write(newComic.getId().toString());
                         break;
 
-                        //res.sendRedirect("/upload?blob_key=" + blobs.get("file0").get(0).getKeyString());
-                    } catch (ParameterNotFoundException | RepeatTitleException ex) {
+                    } catch (NonUniqueGoogleIdException | UserNotFoundException ex) {
                         resp.getWriter().write("{\"error\":" + ex.getMessage() + "}");
 
-                        resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
-                    }
-                    break;
-                case "DELETE ISSUE":
-                    try {
-                        String comicId = req.getParameter("comicId");
-                        String issueTitle = req.getParameter("issueTitle");
-
-                        if (comicId == null) {
-                            throw new ParameterNotFoundException("comicId");
-                        }
-                        if (issueTitle == null) {
-                            throw new ParameterNotFoundException("comicId");
-                        }
-
-                        Long id = Long.valueOf(comicId);
-                        WebComic comic = ComicAccess.queryForComic(id);
-                        boolean success = comic.getChildMediaList().get(0).removeFromChildMediaList(issueTitle);
-                        if(!success) throw new ParameterNotFoundException("Issue named" + issueTitle);
-
-                        break;
+                        resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
                     } catch (ParameterNotFoundException ex) {
                         resp.getWriter().write("{\"error\":" + ex.getMessage() + "}");
 
                         resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
                     }
+                } else {
+                    resp.getWriter().write("{\"error\":\"No User Logged In\"}");
 
-            }
+                    resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+                }
+                break;
+
+            case "ADD ISSUE":
+                try {
+                    Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(req);
+
+                    String title = req.getParameter("issueTitle");
+                    String comicId = req.getParameter("comicId");
+
+                    if (title == null) {
+                        throw new ParameterNotFoundException("issueTitle");
+                    }
+                    if (comicId == null) {
+                        throw new ParameterNotFoundException("comicId");
+                    }
+
+                    Long id = Long.valueOf(comicId);
+                    WebComic comic = ComicAccess.queryForComic(id);
+                    ComicChapter myChapter = comic.getChildMediaList().get(0);
+
+                    if (!isUniqueName(myChapter.getChildMediaList(), title)) {
+                        throw new RepeatTitleException(title);
+                    }
+
+                    for (String n : blobs.keySet()) {
+                        String file = blobs.get(n).get(0).getKeyString();
+                        ComicPage newPage = new ComicPage(title, file);
+                        myChapter.addToChildMediaList(newPage);
+                    }
+
+                    myChapter.reload();
+                    ObjectifyHelper.save(comic);
+
+                    comic.reload();
+                    comic.getMetadata().reload();
+                    resp.getWriter().write(comic.getId().toString());
+
+                    break;
+
+                    //res.sendRedirect("/upload?blob_key=" + blobs.get("file0").get(0).getKeyString());
+                } catch (ParameterNotFoundException | RepeatTitleException ex) {
+                    resp.getWriter().write("{\"error\":" + ex.getMessage() + "}");
+
+                    resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+                }
+                break;
+            case "DELETE ISSUE":
+                try {
+                    String comicId = req.getParameter("comicId");
+                    String issueTitle = req.getParameter("issueTitle");
+
+                    if (comicId == null) {
+                        throw new ParameterNotFoundException("comicId");
+                    }
+                    if (issueTitle == null) {
+                        throw new ParameterNotFoundException("comicId");
+                    }
+
+                    Long id = Long.valueOf(comicId);
+                    WebComic comic = ComicAccess.queryForComic(id);
+                    boolean success = comic.getChildMediaList().get(0).removeFromChildMediaList(issueTitle);
+                    if (!success)
+                        throw new ParameterNotFoundException("Issue named" + issueTitle);
+
+                    break;
+                } catch (ParameterNotFoundException ex) {
+                    resp.getWriter().write("{\"error\":" + ex.getMessage() + "}");
+
+                    resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+                }
 
         }
+
+    }
 /*
     public void doGet(HttpServletRequest req, HttpServletResponse res)
             throws IOException {
@@ -198,12 +195,12 @@ public class Upload extends HttpServlet{
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
         String action = req.getParameter("action");
-        switch(action) {
+        switch (action) {
             case "GET ISSUE NAMES":
                 try {
                     String comicId = req.getParameter("comicId");
 
-                    if(comicId == null) {
+                    if (comicId == null) {
                         throw new ParameterNotFoundException("comicId");
                     }
 
@@ -211,7 +208,7 @@ public class Upload extends HttpServlet{
                     WebComic comic = ComicAccess.queryForComic(id);
                     ComicChapter myChapter = comic.getChildMediaList().get(0);
                     ArrayList<String> issueList = new ArrayList<String>();
-                    for(ComicPage page : myChapter.getChildMediaList()){
+                    for (ComicPage page : myChapter.getChildMediaList()) {
                         issueList.add(page.getName());
                     }
 
@@ -254,13 +251,13 @@ public class Upload extends HttpServlet{
         }
     }
 
-}
-
-private boolean isUniqueName(List<ComicPage> pageList, String name) {
-    for (ComicPage page : pageList) {
-        if (page.getName().equals(name)) {
-            return false;
+    private boolean isUniqueName(List<ComicPage> pageList, String name) {
+        for (ComicPage page : pageList) {
+            if (page.getName().equals(name)) {
+                return false;
+            }
         }
+        return true;
     }
-    return true;
+
 }
